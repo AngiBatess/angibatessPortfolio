33a909218992f99b713927a600dbca056a4ad537
Angila Bates <iu8429rb@my.hennepintech.edu>
2020-05-03 17:27:08 -0500

    Use the tutorial you found to do php on ur contact page :)

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])){
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo = "angibatess@hotmail.com";
+  $mailTo = "angibatess@gmail.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have recieved and e-mail from ".$name.".\n\n".$message;
 
